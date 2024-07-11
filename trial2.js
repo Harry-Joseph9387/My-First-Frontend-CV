e38488673a9b770c19110fba2acd829e0b260a26
Harry-Joseph9387 <harryjoseph9387@gmail.com>
2024-07-11 15:10:22 +0530

    Add files via upload

---
+++ trial2.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const hamburgerspan=document.querySelector(".symbol");
+    const links=document.querySelector(".sub-links");
+    hamburgerspan.classList.toggle("open")
+    links.classList.toggle("open")
+}
+
